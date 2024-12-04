02b7648566594ed2873368f663c08ed7d0efc1e7
Lejla Sukanović <lejla.sukanovic@student.um.si>
2024-12-04 12:41:51 +0100

    select food category bar

--- rvir_projekt/lib/pages/home.dart
+++ rvir_projekt/lib/pages/home.dart
@@ -11,6 +11,9 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home>{
 
+  bool sweetFood=false, fastFood=false, healthyFood=false;
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,10 +41,79 @@ class _HomeState extends State<Home>{
               ),
               Text("We have some delicious food options for you", 
               style: AppWidget.lightTextFieldStyle(),
+              
               ),
+              SizedBox(height: 20.0,),
+              showItem(),
+              
           
       ],)
       ,)
     );
+  }
+
+  Widget showItem(){
+    return Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: (){
+                      sweetFood=true;
+                      fastFood=false;
+                      healthyFood=false;
+                      setState(() {
+                        
+                      });
+                    },
+                    child: Material(
+                      elevation: 5.0,
+                      borderRadius: BorderRadius.circular(10),
+                      child:Container(
+                        decoration: BoxDecoration(color: sweetFood? Colors.black : Colors.white, borderRadius: BorderRadius.circular(10)),
+                        padding: EdgeInsets.all(8),
+                        child: Image.asset("images/ice-cream-icon.png", height: 40, width: 40, fit: BoxFit.cover, color: sweetFood? Colors.white : Colors.black,),
+                      )
+                    ),
+              ),
+                GestureDetector(
+                    onTap: (){
+                      sweetFood=false;
+                      fastFood=true;
+                      healthyFood=false;
+                      setState(() {
+                        
+                      });
+                    },
+                    child: Material(
+                      elevation: 5.0,
+                      borderRadius: BorderRadius.circular(10),
+                      child:Container(
+                        decoration: BoxDecoration(color: fastFood? Colors.black : Colors.white, borderRadius: BorderRadius.circular(10)),
+                        padding: EdgeInsets.all(8),
+                        child: Image.asset("images/burger-icon.png", height: 40, width: 40, fit: BoxFit.cover, color: fastFood? Colors.white : Colors.black,),
+                      )
+                    ),
+              ),
+                GestureDetector(
+                    onTap: (){
+                      sweetFood=false;
+                      fastFood=false;
+                      healthyFood=true;
+                      setState(() {
+                        
+                      });
+                    },
+                    child: Material(
+                      elevation: 5.0,
+                      borderRadius: BorderRadius.circular(10),
+                      child:Container(
+                        decoration: BoxDecoration(color: healthyFood? Colors.black : Colors.white, borderRadius: BorderRadius.circular(10)),
+                        padding: EdgeInsets.all(8),
+                        child: Image.asset("images/salad-icon.png", height: 40, width: 40, fit: BoxFit.cover, color: healthyFood? Colors.white : Colors.black,),
+                      )
+                    ),
+              )
+                
+              ],);
   }
 }
